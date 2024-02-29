3b3b0a004567bfff48f490cce55f37c217e5c069
aminehochlef <hochamine22@gmail.com>
2024-02-29 17:45:02 +0100

    using api endpoints to return Json

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
+
 
 app = Flask(__name__)
 
@@ -25,6 +26,10 @@ JOBS = [
 @app.route('/')
 def home():
     return render_template('home.html', jobs=JOBS, company_name='Jovian')
+
+@app.route('/api/jobs')
+def jobs():
+    return jsonify(JOBS)
 
 if __name__ == "__main__":
     app.run(debug=True)
