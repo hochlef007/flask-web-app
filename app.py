bce7b40eb05fef7298bc852f8b8539fc796d8e38
aminehochlef <hochamine22@gmail.com>
2024-02-29 17:35:35 +0100

    dynamic data using Jinja

--- app.py
+++ app.py
@@ -2,9 +2,29 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+JOBS = [
+    {
+        'id': 1,
+        'title': 'Software Engineer',
+        'location': 'New York, NY',
+        'salary': '24k dollars'
+    },
+    {
+        'id': 2,
+        'title': 'Data Scientist',
+        'location': 'Tokyo, Japan',
+        'salary': '24k dollars'
+    },
+    {
+        'id': 3,
+        'title': 'UX/UI Designer',
+        'location': 'Tunis, Tunisia',
+    }
+]
+
 @app.route('/')
-def hello_world():
-    return render_template('home.html')
+def home():
+    return render_template('home.html', jobs=JOBS, company_name='Jovian')
 
 if __name__ == "__main__":
     app.run(debug=True)
